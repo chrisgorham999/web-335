251aab9edd2b40fbdd25d6a727f0109cde6c8d6d
Chris Gorham <chris.gorham@outlook.com>
2023-03-20 12:51:40 -0400

    week 2 work

---
+++ week-2/gorham_myworld.py
@@ -0,0 +1,10 @@
+
+#======================================
+# Title: gorham_myworld.py 
+# Author: Chris Gorham
+# Date: 20 Mar 2023
+# Description: This code writes our first Python program
+# Sources Used:  N/A
+#=====================================
+
+print ("Your are now in Gorham's world!")
