861edfd1ed3c2894acf0cce2f14a86627269db17
Chris Gorham <chris.gorham@outlook.com>
2023-03-27 17:22:26 -0400

    Update gorham_calculator.py

--- week_3/gorham_calculator.py
+++ week_3/gorham_calculator.py
@@ -28,7 +28,7 @@ num2 = "6"
 num3 = add(num1, num2)
 
 #create the string for the add demonstration
-add_demo = "%s + %s = %s" % (num1, num2, num3)
+add_demo = "%s + %s is %s." % (num1, num2, num3)
 #print the add demonstration string
 print(add_demo)
 
@@ -38,7 +38,7 @@ num5 = "4"
 num6 = subtract(num4, num5)
 
 #create the string for the subtract demonstration
-subtract_demo = "%s - %s = %s" % (num4, num5, num6)
+subtract_demo = "%s - %s is %s." % (num4, num5, num6)
 #print the subtract demonstration string
 print(subtract_demo)
 
@@ -48,7 +48,7 @@ num8 = "4"
 num9 = divide(num7, num8)
 
 #create the string for the divide demonstration
-divide_demo = "%s / %s = %s" % (num7, num8, num9)
+divide_demo = "%s / %s is %s." % (num7, num8, num9)
 #print the divide demonstration string
 print(divide_demo)
 
@@ -58,7 +58,7 @@ num11 = "4"
 num12 = multiply(num10, num11)
 
 #create the string for the multiply demonstration
-multiply_demo = "%s * %s = %s" % (num10, num11, num12)
+multiply_demo = "%s * %s is %s." % (num10, num11, num12)
 #print the multiply demonstration string
 print(multiply_demo)
 
